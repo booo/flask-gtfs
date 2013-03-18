60e06deeef692b9d5003abee7ae3f8f30a732327
Knut <knut@k-nut.eu>
2013-03-18 19:27:27 +0100

    Show some stops in the linegrpah

--- static/js/linegraph.js
+++ static/js/linegraph.js
@@ -1,6 +1,16 @@
 var onLoad = function onLoad() {
   console.log("foo");
   $.getJSON('./api/trips/20869792/stops', function(data){
+    var stops = [];
+    $.each(data.stops,
+      function(){
+        stops.push("<li>" + this.name + " </li>");
+      }
+      );
+    $('<ul/>', {
+      'class': 'my-new-list',
+      html: stops.join('')
+    }).appendTo('body');
     console.log(data);
   });
 };
